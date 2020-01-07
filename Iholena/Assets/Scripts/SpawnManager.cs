4354abf4c35efaf8110ab54925ea01f27ee69d74
jmacnut <jmacnut@gmail.com>
2020-01-07 04:52:40 -1000

    ti leaf now moves in x and y directions and is bounded

---
+++ Iholena/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+   [SerializeField]
+   private GameObject _invaderPrefab;
+
+   //[SerializeField]
+   //private GameObject _invaderContainer;
+
+   //[SerializeField]
+   //private GameObject[] powerups;
+
+   private bool _stopSpawning = false;
+
+   public void StartSpawning()
+   {
+      StartCoroutine(SpawnInvaderRoutine());
+      //StartCoroutine(SpawnPowerupRoutine());
+   }
+
+   IEnumerator SpawnInvaderRoutine()
+   {
+      yield return new WaitForSeconds(3.0f);
+
+      while (_stopSpawning == false)
+      {
+         float waitTime = 5.0f;
+
+      Vector3 posToSpawn = new Vector3(Random.Range(-9.5f, 9.5f), 10.0f, 0);
+      Instantiate(_invaderPrefab, posToSpawn, Quaternion.identity);   // tmp
+      //   GameObject newInvader = Instantiate(_invaderPrefab, posToSpawn, Quaternion.identity);
+      //   newInvader.transform.parent = _invaderContainer.transform;
+
+      yield return new WaitForSeconds(waitTime);
+      }
+   }
+
+   //IEnumerator SpawnPowerupRoutine()
+   //{
+   //   yield return new WaitForSeconds(3.0f);
+
+   //   while (_stopSpawning == false)
+   //   {
+   //      float waitTime = Random.Range(15.0f, 21.0f);
+
+   //      Vector3 posToSpawn = new Vector3(Random.Range(-9.5f, 9.5f), 10.0f, 0);
+   //      int randomPowerUp = Random.Range(0, 3);   // powerups 0, 1, and 2
+   //      Instantiate(powerups[randomPowerUp], posToSpawn, Quaternion.identity);
+
+   //      yield return new WaitForSeconds(waitTime);
+   //   }
+   //}
+
+   //public void OnPlayerDeath()
+   //{
+   //   _stopSpawning = true;
+   //}
+
+}
--- Iholena/Assets/Scripts/TiLeaf.cs
+++ Iholena/Assets/Scripts/TiLeaf.cs
@@ -2,44 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// ToDO:
+// - move player in x and y directions
+// - optimize performance (jerky player movement)
+// - refactor code - generif move() method
+
 public class TiLeaf : MonoBehaviour
 {
    [SerializeField]
-   private float _speed;
+   private float _speed = 5.0f;
    private float _rightLimit = 14.5f;
    private float _leftLimit = -14.5f;
+   private float _verticalLimit = -8.3f;
+   private float _horizontalPos;
+   private float _verticalPos;
 
-   // Start is called before the first frame update
-   void Start()
-   {
-      _speed = 5.0f;
-   }
-
-   // Update is called once per frame
    void Update()
    {
-      //float horizontalPos;
-      float verticalPos = -4.5f;
-      Vector3 newPosition;
+      _horizontalPos = Input.GetAxis("Horizontal");
+      _verticalPos = Input.GetAxis("Vertical");
+      Vector3 newPosition = new Vector3(_horizontalPos, _verticalPos, 0);
 
-      if (Input.GetKey(KeyCode.RightArrow))
-      {
-         if (transform.position.x >= _rightLimit)
-         {
-            newPosition = new Vector3(_leftLimit, verticalPos, 0);
-            transform.position = newPosition;
-         }
-         transform.Translate(Vector3.right * _speed * Time.deltaTime);
-      }
+      transform.Translate(newPosition * _speed * Time.deltaTime);
 
-      if (Input.GetKey(KeyCode.LeftArrow))
+
+      if (transform.position.x >= _rightLimit)
       {
-         if (transform.position.x <= _leftLimit)
-         {
-            newPosition = new Vector3(_rightLimit, verticalPos, 0);
-            transform.position = newPosition;
-         }
-         transform.Translate(Vector3.left * _speed * Time.deltaTime);
+         newPosition = new Vector3(_leftLimit, _verticalLimit, 0);
+         transform.position = newPosition;
       }
+      transform.Translate(Vector3.right * _speed * Time.deltaTime);
+
+      if (transform.position.x <= _leftLimit)
+      {
+         newPosition = new Vector3(_rightLimit, _verticalLimit, 0);
+         transform.position = newPosition;
+      }
+      transform.Translate(Vector3.left * _speed * Time.deltaTime);
+
    }
 }
